f09747bc7684333eae32c448bdb6b9347da98e13
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-04-16 03:48:08 +0200

    feat: add Queue solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const removeKFromList = (l, k) => {
 }
 
 module.exports = {
