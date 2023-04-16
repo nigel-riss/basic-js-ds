85547881695c3ea11f1a484ab77694e9e51978f8
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-04-17 00:04:05 +0200

    feat: add removeKFromList solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -23,7 +21,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 const removeKFromList = (l, k) => {
+  let cleanHead = new ListNode(null)
+  let cleanTail = cleanHead
+  let pointer = l
+
+  while(pointer) {
+    if (pointer.value !== k) {
+      cleanTail.next = new ListNode(pointer.value)
+      cleanTail = cleanTail.next
+    }
+
+    pointer = pointer.next
+  }
+
+  return cleanHead.next
 }
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+const initial = convertArrayToList([1, 2, 3]);
+const expected = convertArrayToList([1, 2]);
+console.log(removeKFromList(initial, 3), expected);
 
 module.exports = {
   removeKFromList
